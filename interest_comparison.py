6e2f54daebcffcac0b7705a3d424f1b749670af6
mark-codes-stuff <mark.91.codes@gmail.com>
2024-05-29 12:39:47 +0100

    updated

--- interest_comparison.py
+++ interest_comparison.py
@@ -1,29 +1,33 @@
-#Script to compare 2 savings interest rates and format the monthly and annual interest in a table
+#Script to compare 2 savings interest rates, and format the monthly and annual interest & difference in a table
 
+#Interest and balance variables, including a modified variable which will be used when formatting the original instead of changing it
 interest_1 = input("First interest rate, either as a percentage or a decimal \n")
+formatted_interest_1 = 0
 interest_2 = input("Second interest rate, either as a percentage or a decimal \n")
+formatted_interest_2 = 0
 
 balance = input("Balance to calculate against, as a number \n")
+formatted_balance = 0
 
+percentage_symbol = "%"
 character_to_remove = ['%', '£', '$', '€']
 decimal_check = '.'
 
 check_complete = False
 
 #function to validate input and format the strings correctly before turning interests into floats
-#these checks don't currently work as the iteration is wrong, to fix on next look
 while check_complete is False:
-    for character_to_remove in interest_1: #remove a forbidden character before converting to float
-        interest_1 = interest_1.replace(character_to_remove, '')
+    for char in character_to_remove: #remove a forbidden character before converting to float
+        interest_1 = interest_1.replace(char, '')
         
-    for character_to_remove in interest_2: #remove a forbidden character before converting to float
-        interest_2 = interest_2.replace(character_to_remove, '')
+    for char in character_to_remove: #remove a forbidden character before converting to float
+        interest_2 = interest_2.replace(char, '')
         
-    if balance.isnumeric() == False: #check the balance is a number and not text
+    while balance.isnumeric() == False: #check the balance is a number and not text
         balance = input("Please re-enter balance as number \n")
     
-    for character_to_remove in balance:
-        balance = balance.replace(character_to_remove, '')
+    for char in character_to_remove:
+        balance = balance.replace(char, '')
     
 
 
